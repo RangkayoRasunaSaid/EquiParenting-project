b97062bb64da8589c375609112678d5718428ead
Diah Junaidi <j.fdiah@gmail.com>
2024-01-31 09:30:32 +0700

    update router dan beberapa source code di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './LandingPage.css';
-import HeroSection from './HeroSection';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import './App.css'
+import './components/landing-page/LandingPage.css';
+import LandingPage from './components/landing-page/index';
 
 function App() {
-
   return (
+    <Provider store={store}>
     <>
-    <HeroSection></HeroSection>
+      <LandingPage />
+      {/* Add other components or routes here */}
     </>
-  )
+  </Provider>
+  );
 }
 
 export default App
