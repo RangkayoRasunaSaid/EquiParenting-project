6d7c6884e9d3d0ed729d29ce07c69feae15d03db
Diah Junaidi <j.fdiah@gmail.com>
2024-01-31 09:28:39 +0700

    update library

---
+++ src/FeatureSection.jsx
@@ -0,0 +1,16 @@
+const FeatureSection = () => {
+    return(
+        <>
+                <div className="container-fluid mb-4" id="secondSection">
+                    <div className="row justify-content-center text-center">
+                        <div className="col-12 col-md-8">
+                        <h3>Belajar & Berkembang</h3>
+                        <p>Dapatkan tips dan aktivitas berdasarkan usia anak</p>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
+};
+export default FeatureSection;
