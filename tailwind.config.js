f01bd29da04b894b783bec8c71fef6e81a7b6d88
Nazilah17 <nazilah0000@gmail.com>
2024-02-06 02:00:22 +0800

    satukan custom warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,23 +11,21 @@ export default {
       colors: {
         ungu1: "#675893",
         ungu2: "#E8DEF7",
+        primary: "#675893",
+        secondary: "#E8DEF7",
+        tertiery: "#7f508f",
       },
       fontSize: {
         xxs: "0.65rem",
       },
       backgroundColor: {
-        'main-color': '#675893',
-        'light-main-color': '#776493',
-        'dark-main-color': '#392c60',
+        "main-color": "#675893",
+        "light-main-color": "#776493",
+        "dark-main-color": "#392c60",
       },
-      colors: {
-        "primary": "#675893",
-        "secondary": "#E8DEF7",
-        "tertiery": "#7f508f",
-      },
-      fontWeight: ['hover', 'focus', 'active', 'group-hover']
+      fontWeight: ["hover", "focus", "active", "group-hover"],
     },
-    
+
     // container: {
     //   center: true,
     //   padding: {
