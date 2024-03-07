aba80032d01b008a043fd3776f2c7a67db0e1490
unknown <diandraregita14@gmail.com>
2024-03-07 10:04:52 +0700

    spin loaders

--- src/components/mission/PusatReward.jsx
+++ src/components/mission/PusatReward.jsx
@@ -6,7 +6,7 @@ import ModalButton from "./modals/ModalButton";
 import ModalSpin from "./modals/ModalSpin";
 import { Link } from 'react-router-dom';
 import StatSummary from './StatSummary.jsx';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import img1 from '../../assets/8.png'
 import img2 from '../../assets/7.png'
@@ -15,19 +15,27 @@ import { useDispatch, useSelector } from 'react-redux';
 import { ClipLoader } from 'react-spinners';
 import { override } from '../../pages/Profile.jsx';
 
+const options = {
+    stagePadding: 40, items: 2, margin:20, nav:true,
+    responsive:{ 0:{ items:1 }, 600:{ items:2 }, 1000:{ items:2 }
+    }
+}
+
 export default function PusatReward({ setUpdateMembers }) {
     const dispatch = useDispatch();
-    const { stats, error, loading } = useSelector(state => state.stats)
+    const { stats, loading } = useSelector(state => state.stats)
     const { members } = useSelector(state => state.member)
     let endDate
+
     if (members && members.length > 0) {
       endDate = new Date(members[0].Rewards[0]?.end_date)
       endDate.setTime(endDate.getTime() - (7 * 60 * 60 * 1000))
     }
+
     let currentDate = new Date();
     let keysWith100Percentage
     let spinMembers = []
-    let allRolesUnique
+    let allRolesUnique = true
    
     useEffect(() => {
         const fetchMemberActivityStats = async () => {
@@ -41,13 +49,12 @@ export default function PusatReward({ setUpdateMembers }) {
         if (!sessionStorage.getItem('token') || !members || members.length === 0 || members[0].Rewards.length === 0) return
         fetchMemberActivityStats()
     }, [members]);
-   
-    useEffect(() => {
-        if (!stats) return
+
+    if (stats) {
         keysWith100Percentage = Object.keys(stats).filter(key => stats[key].percentage === 100);
         spinMembers = members.filter(member => keysWith100Percentage.includes(member.id.toString()) && member.Rewards.some(reward => reward.Reward_Items.length === 0));
         allRolesUnique = new Set(members.map(member => member.member_role)).size === members.length;
-    }, [stats]);
+    }
 
     const btnCtn = (
         <img
@@ -58,22 +65,13 @@ export default function PusatReward({ setUpdateMembers }) {
         />
     ) 
 
-    const options = {
-        stagePadding: 40, items: 2, margin:20, nav:true,
-        responsive:{ 0:{ items:1 }, 600:{ items:2 }, 1000:{ items:2 }
-        }
-    }
-
-      return (
+    return (
         <div className="text-center bg-white sm:p-5 md:p-10 p-3 rounded-[60px] flex flex-col justify-center">
             <h1 className="text-3xl font-bold">Pusat Reward</h1>
-            {!stats || !members ? (
-                <ClipLoader color="silver" loading={loading} cssOverride={override} size={50} aria-label="Loading Spinner" data-testid="loader" />
-            ) : (
                 <>
                     <div className="bg-white m-4 rounded-[60px] shadow-md flex-none lg:flex">
                         <div className="lg:w-1/3 sm:w-full flex justify-center">
-                            {sessionStorage.getItem("token") ? (
+                            {sessionStorage.getItem("token") && members ? (
                                 members.length === 0 || endDate > currentDate || spinMembers.length === 0 ? (
                                     <div onClick={() => {
                                         if (endDate > currentDate) toast.warning('Belum Bisa Putar Spin karena Periode Masih Berjalan')
@@ -87,7 +85,7 @@ export default function PusatReward({ setUpdateMembers }) {
                                 <Link to='/login'>{btnCtn}</Link>
                             )}
                         </div>
-                        {members.length === 0 ? (
+                        {!members || members.length === 0 ? (
                             <div className='flex flex-col justify-center items-center lg:mx-auto'>
                                 <Link to={sessionStorage.getItem("token") ? "/mission/daily-mission" : '/login'}>
                                     <button onClick={() => window.scrollTo(0, 0)} className='flex items-center justify-center px-5 py-3 rounded-3xl shadow-lg text-xl font-semibold'>
@@ -95,6 +93,8 @@ export default function PusatReward({ setUpdateMembers }) {
                                     </button>
                                 </Link>
                             </div>
+                        ) : !stats || !members ? (
+                            <ClipLoader color="silver" loading={loading} cssOverride={override} size={50} aria-label="Loading Spinner" data-testid="loader" />
                         ) : (
                             <OwlCarousel className='owl-theme lg:w-2/3 my-10' {...options} >
                                 {members.map(member => (
@@ -106,7 +106,8 @@ export default function PusatReward({ setUpdateMembers }) {
                                     />
                                 ))}
                             </OwlCarousel>
-                        )}
+                        )
+                        }
                     </div>
             
                     {/* link to daily mission */}
@@ -114,14 +115,13 @@ export default function PusatReward({ setUpdateMembers }) {
                         <button className="p-sm-3 p-md-4 mb-4 p-4 mt-8 text-white bg-main-color rounded-[60px] font-bold text-[16px] md:text-xl shadow-md w-full" onClick={() => window.scrollTo(0, 0)}>Lihat Aktivitas Daily Mission</button>
                     </Link>
 
-                    {members.filter(member => member.Rewards[0]).length > 0 &&
-                        <>
-                            <h1 className='mt-5 text-center text-3xl font-bold'>Ringkasan</h1>
-                            <StatSummary members={members} stats={stats} />
-                        </>
-                    }
+                    <h1 className='mt-5 text-center text-3xl font-bold'>Ringkasan</h1>
+                    {!stats || !members ? (
+                        <ClipLoader color="silver" loading={loading} cssOverride={override} size={50} aria-label="Loading Spinner" data-testid="loader" />
+                    ) : members.filter(member => member.Rewards[0]).length > 0 &&(
+                        <StatSummary members={members} stats={stats} />
+                    )}
                 </>
-            )}
 
         </div>
     )
